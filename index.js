bda2b6942c5967abeb3297ef1c31673bb4a25aef
Maksim Prokopenko <maksim.prokopenko@m-brain.com>
2018-03-12 13:46:34 +0200

    Cleaned unused console log

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const logReport = [];
 const whatHappendMiddleware = store => next => (action) => {
-  if (action.type === 'WHAT_HAPPEND_REPORT') {
-    const data = new Blob([JSON.stringify(logReport)], { type: 'application/json' });
-    const csvURL = window.URL.createObjectURL(data);
-    const tempLink = document.createElement('a');
-    tempLink.href = csvURL;
-    tempLink.setAttribute('download', 'report.json');
-    tempLink.click();
+  if((!process.env.NODE_ENV || process.env.NODE_ENV === 'development')) {
+    if (action.type === 'WHAT_HAPPEND_REPORT') {
+        const data = new Blob([JSON.stringify(logReport)], { type: 'application/json' });
+        const csvURL = window.URL.createObjectURL(data);
+        const tempLink = document.createElement('a');
+        tempLink.href = csvURL;
+        tempLink.setAttribute('download', 'report.json');
+        tempLink.click();
+    }
+    const happends = {};
+    happends.prev_state = store.getState();
+    happends.action = action;
+    next(action);
+    happends.next_state = store.getState();
+    logReport.push(happends);
   }
-  const happends = {};
-  happends.prev_state = store.getState();
-  happends.action = action;
-  next(action);
-  happends.next_state = store.getState();
-  logReport.push(happends);
-  console.log(logReport);
 };
 
 export default whatHappendMiddleware;
